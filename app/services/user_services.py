63f8475969675c4ccbf1bed02761558ad5498555
Rahul Reekhadi <rahulreekhadi87@gmail.com>
2025-12-22 23:13:05 +0530

    login and signup routes created with jwt auth

--- app/services/user_services.py
+++ app/services/user_services.py
@@ -1,22 +1,47 @@
 from app import db
 from app.models.user import User
 from sqlalchemy.exc import IntegrityError
-from werkzeug.exceptions import BadRequest, Conflict, NotFound
+from werkzeug.exceptions import BadRequest, Conflict, NotFound, Unauthorized
+from flask_jwt_extended import create_access_token
 
 
-def create_user(data):
-    if not data.get("name") or not data.get("email"):
-        raise BadRequest("Name and email are required")
+def signup(data):
+    if not data.get("name") or not data.get("email") or not data.get("password"):
+        raise BadRequest("Name, Password and email are required")
 
     user = User(name=data["name"], email=data["email"])
+    user.set_password(data["password"])
     db.session.add(user)
-
+    print("what is the behaviour")
     try:
         db.session.commit()
-        return user
     except IntegrityError:
         db.session.rollback()
         raise Conflict("Email already exists")
+    
+    access_token = create_access_token(identity=str(user.id))
+
+    return {
+        "user": user.to_dict(),
+        "access_token": access_token
+    }
+    
+
+def login(data):
+    if not data.get("email") or not data.get("password"):
+        raise BadRequest("Email and password are required")
+
+    user = User.query.filter_by(email=data["email"]).first()
+
+    if not user or not user.check_password(data["password"]):
+        raise Unauthorized("Invalid email or password")
+
+    access_token = create_access_token(identity=str(user.id))
+
+    return {
+        "user": user.to_dict(),
+        "access_token": access_token
+    }
 
 
 def get_all_users():
