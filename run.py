fc4bdb61dc09ead5d5217368fbb9b9e5999b60aa
Rahul Reekhadi <rahulreekhadi87@gmail.com>
2025-12-29 16:08:24 +0530

    login and signup page done

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app import create_app
+from flask_cors import CORS
 
 app = create_app()
-
+CORS(app, 
+     resources={r"/*": {"origins": "http://localhost:3000"}}, 
+     supports_credentials=True,
+     expose_headers=["Content-Type", "Authorization"],
+     allow_headers=["Content-Type", "Authorization"])
 if __name__ == "__main__":
     app.run()
